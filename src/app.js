c907b911fee0411c7ae1a3dd0c1e5b8747e472df
Ognjen Vucetic <ognjenvucko@gmail.com>
2017-10-09 23:56:59 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+
+let rootElement = document.getElementById('root')
+
+ReactDOM.render(<Header title="ReactJs is awesome!"></Header>, rootElement);
